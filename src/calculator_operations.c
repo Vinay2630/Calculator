a6bb63cf274f8681b074b807f4e5c071ef507783
Vinay2630 <61010254+Vinay2630@users.noreply.github.com>
2020-09-18 23:12:14 +0530

    Update calculator_operations.c

--- src/calculator_operations.c
+++ src/calculator_operations.c
@@ -23,6 +23,14 @@ int divide(int operand1, int operand2)
         return operand1 / operand2;
 }
 
+int modulo(int operand1, int operand2)
+{
+    if(0 == operand2)
+        return 0;
+    else
+        return operand1 % operand2;
+}
+
 int power(int operand1, int operand2)
 {
     if(0 == operand2)
